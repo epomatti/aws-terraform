d8a586af5a4dcfd32b4804eebd67d9576c816452
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-07-10 19:31:01 -0300

    files

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -54,6 +54,18 @@ data "aws_iam_policy_document" "s3_policy" {
   }
 }
 
+resource "aws_s3_object" "index" {
+  bucket         = aws_s3_bucket.bucket.bucket
+  key            = "index.html"
+  content_base64 = filebase64("${path.module}/index.html")
+}
+
+resource "aws_s3_object" "saturn5" {
+  bucket         = aws_s3_bucket.bucket.bucket
+  key            = "saturn5.jpg"
+  content_base64 = filebase64("${path.module}/saturn5.jpg")
+}
+
 ### CloudFront ###
 
 // Origin Access Identity
@@ -61,107 +73,58 @@ resource "aws_cloudfront_origin_access_identity" "main" {
   comment = "S3 CloudFront OAI"
 }
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
-#     origin_id   = local.origin_id
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = local.origin_id
 
-#     s3_origin_config {
-#       origin_access_identity = aws_cloudfront_origin_access_identity.main.cloudfront_access_identity_path
-#     }
-#   }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.main.cloudfront_access_identity_path
+    }
+  }
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   comment             = "Some comment"
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "My CloudFront"
+  default_root_object = "index.html"
 
-#   logging_config {
-#     include_cookies = false
-#     bucket          = "mylogs.s3.amazonaws.com"
-#     prefix          = "myprefix"
-#   }
+  # logging_config {
+  #   include_cookies = false
+  #   bucket          = "mylogs.s3.amazonaws.com"
+  #   prefix          = "myprefix"
+  # }
 
-#   aliases = ["mysite.example.com", "yoursite.example.com"]
+  # aliases = ["mysite.example.com", "yoursite.example.com"]
 
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  default_cache_behavior {
+    allowed_methods  = ["GET"]
+    cached_methods   = ["GET"]
+    target_origin_id = local.origin_id
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#   # Cache behavior with precedence 0
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/immutable/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-#     target_origin_id = local.s3_origin_id
+  price_class = "PriceClass_200"
 
-#     forwarded_values {
-#       query_string = false
-#       headers      = ["Origin"]
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["BR"]
+    }
+  }
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-
-#   # Cache behavior with precedence 1
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = false
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-
-#   price_class = "PriceClass_200"
-
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "whitelist"
-#       locations        = ["US", "CA", "GB", "DE"]
-#     }
-#   }
-
-#   tags = {
-#     Environment = "production"
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
