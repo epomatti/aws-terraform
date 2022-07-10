0981e5857fce19a976bfb6f150f428d51ea4d4cc
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-07-10 18:24:16 -0300

    s3, policies, oai

---
+++ cloudfront/main.tf
@@ -0,0 +1,167 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.22.0"
+    }
+  }
+  backend "local" {
+    path = "./.workspace/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+}
+
+### Locals ###
+locals {
+  origin_id = "s3-origin"
+}
+
+### S3 ###
+resource "aws_s3_bucket" "bucket" {
+  bucket = "bucket-cloudfront-999"
+}
+
+resource "aws_s3_bucket_acl" "default" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+resource "aws_s3_bucket_policy" "cloudfront_oai" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.main.iam_arn]
+    }
+  }
+}
+
+### CloudFront ###
+
+// Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "main" {
+  comment = "S3 CloudFront OAI"
+}
+
+# resource "aws_cloudfront_distribution" "s3_distribution" {
+#   origin {
+#     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+#     origin_id   = local.origin_id
+
+#     s3_origin_config {
+#       origin_access_identity = aws_cloudfront_origin_access_identity.main.cloudfront_access_identity_path
+#     }
+#   }
+
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   comment             = "Some comment"
+#   default_root_object = "index.html"
+
+#   logging_config {
+#     include_cookies = false
+#     bucket          = "mylogs.s3.amazonaws.com"
+#     prefix          = "myprefix"
+#   }
+
+#   aliases = ["mysite.example.com", "yoursite.example.com"]
+
+#   default_cache_behavior {
+#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = local.s3_origin_id
+
+#     forwarded_values {
+#       query_string = false
+
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+
+#     viewer_protocol_policy = "allow-all"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
+
+#   # Cache behavior with precedence 0
+#   ordered_cache_behavior {
+#     path_pattern     = "/content/immutable/*"
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#     target_origin_id = local.s3_origin_id
+
+#     forwarded_values {
+#       query_string = false
+#       headers      = ["Origin"]
+
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+
+#     min_ttl                = 0
+#     default_ttl            = 86400
+#     max_ttl                = 31536000
+#     compress               = true
+#     viewer_protocol_policy = "redirect-to-https"
+#   }
+
+#   # Cache behavior with precedence 1
+#   ordered_cache_behavior {
+#     path_pattern     = "/content/*"
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = local.s3_origin_id
+
+#     forwarded_values {
+#       query_string = false
+
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#     compress               = true
+#     viewer_protocol_policy = "redirect-to-https"
+#   }
+
+#   price_class = "PriceClass_200"
+
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "whitelist"
+#       locations        = ["US", "CA", "GB", "DE"]
+#     }
+#   }
+
+#   tags = {
+#     Environment = "production"
+#   }
+
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
+# }
